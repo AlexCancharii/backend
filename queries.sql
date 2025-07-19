3ca53db82d00514850b5bcd92c1aaa118550ce45
Sebastian <sebastianchavarry2001@gmail.com>
2025-07-19 17:29:01 -0500

    queries2

---
+++ queries.sql
@@ -0,0 +1,25 @@
+CREATE TABLE exercise (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR NOT NULL UNIQUE,
+  muscle_group VARCHAR,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE workout_session (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
+  started_at TIMESTAMP NOT NULL,
+  ended_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+CREATE TABLE series (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  session_id UUID REFERENCES workout_session(id) ON DELETE CASCADE,
+  exercise_id UUID REFERENCES exercise(id),
+  set_number INTEGER NOT NULL,
+  reps INTEGER NOT NULL,
+  weight NUMERIC(5,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
